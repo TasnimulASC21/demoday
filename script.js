11ce9492b4de671466e07c7cdaef7b2312ec124e
TasnimulASC21 <tislam@mecps.org>
2021-08-16 09:22:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let big_div = document.getElementById("tutorial");
+let tutorial_header = document.createElement("header");
+tutorial_header.classList = "tutorial-header";
+tutorial_header.innerHTML = "Easy Steps Towards Success";
+big_div.appendChild(tutorial_header);
